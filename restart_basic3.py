6dd0d06c9a4a363760cf3afbebad6cd024aebbff
ryu-jihye <74997421+ryu-jihye@users.noreply.github.com>
2020-12-17 23:27:11 +0900

    Add files via upload

---
+++ restart_basic3.py
@@ -0,0 +1,27 @@
+# 실행결과
+# 437420
+
+앉힐수있는최소사람의수 = 2
+앉힐수있는최대사람의수 = 10
+전체사람의수 = 100
+memo = {}
+
+def 문제(남은사람수, 앉힌사람수):
+    key = str([남은사람수, 앉힌사람수])
+    #종료조건
+    if key in memo:
+        return memo[key]
+    if 남은사람수 < 0:
+        return 0
+    if 남은사람수 == 0:
+        return 1
+    
+    count = 0
+    for i in range(앉힌사람수,앉힐수있는최대사람의수 + 1):
+        count += 문제(남은사람수 - i, i)
+
+    memo[key] = count
+    
+    return count
+
+print(문제(전체사람의수, 앉힐수있는최소사람의수))
